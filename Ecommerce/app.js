5e59dd7b80af9142bbb3953f793615e0e02eaa75
akshayraj-1 <akshayraj4101@gmail.com>
2024-03-02 00:12:39 +0530

    Updated Ecommerce Project

--- Ecommerce/app.js
+++ Ecommerce/app.js
@@ -5,29 +5,35 @@ import fs from 'fs';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3001;
 
-app.get("/", async (req, res) => {
-    try {
-        const data = fs.readFileSync(path.join(__dirname, './src/data/products.json'), 'utf-8');
-        const products = await JSON.parse(data);
-        if (!req.query.hasOwnProperty('category')) {
-            return res.status(200).send('<h3 style="font-family: sans-serif;">Usage:<br>/?category=food</h3>');
+app.get("/", (req, res) => {
+    if (!req.query.hasOwnProperty('category')) {
+        res.status(200).sendFile(path.join(__dirname, './src/index.html'));
+        return;
+    }
+
+    fs.readFile(path.join(__dirname, './src/data/products.json'), 'utf-8', (error, result) => {
+        if (error) {
+            res.status(500).send(error);
+            return;
         }
+        const products = JSON.parse(result) || [];
         switch (req.query.category.toLowerCase()) {
             case 'food':
-                res.json(products.filter((product) => product.category === "food"));
+                res.status(200).json(products.filter((product) => product.category === "food"));
                 break;
             case 'others':
                 res.status(200).json(products.filter((product) => product.category === "others"));
                 break;
             default:
-                res.status(200).json(products);
+                res.status(204).send("No Products Found");
         }
+    });
+});
 
-    } catch (error) {
-        res.status(500).send(error);
-    }
-})
+app.get('/data/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './src/', req.url));
+});
 
-app.listen(port, (error) => console.log(error ? 'Unable to start the server' : 'Server started...'));
+app.listen(PORT, (error) => console.log(error ? 'Unable to start the server' : 'Server started...'));
